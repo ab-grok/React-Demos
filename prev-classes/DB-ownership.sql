08c635c94c21dbb5a8f0922c602c25d5e23c415c
AB <absule032@gmail.com>
2026-06-06 14:55:14 +0100

    DB-ownership 2

--- prev-classes/DB-ownership.sql
+++ prev-classes/DB-ownership.sql
@@ -1,19 +1,46 @@
--- 1) ===> Query to check the ownership of schemas in the database
+-- 1) ===> Creating a new role (group);
+CREATE ROLE <role_name> NOLOGIN;
+
+
+-- 2) ===> Add roles to role group
+GRANT <role_name> TO <role_name1>, <role_name2>;
+
+
+-- 3) ===> Change database ownership
+ALTER DATABASE shooter OWNER TO <role_name>;
+
+
+-- 3) ===> Change schema ownership
+ALTER SCHEMA <schema_name> OWNER TO <role_name>;
+
+
+-- 4) ===> Query to check the ownership of current database;
+SELECT pg_get_userbyid(datdba)
+FROM pg_database
+WHERE datname = current_database();
+
+
+-- 5) ===> Query to check the ownership of schemas in the database
 SELECT nspname AS schema,
        pg_get_userbyid(nspowner) AS owner -- pg_get_userbyid() returns the username for the id in nspowner column;
-FROM pg_namespace
+FROM pg_namespace 
+WHERE nspname NOT LIKE 'pg_%'
+AND nspname <> 'information_schema'                 
 ORDER BY schema;
 
--- 2) ===> Query to check the ownership of tables in a specific schema
-SELECT current_database() as database_name, schemaname,tablename, tableowner FROM pg_tables where schemaname not like 'pg_%' ORDER BY schemaname, tablename;
 
--- _______________________________________________________
--- 3)  ===> Change schema ownership
-ALTER SCHEMA <schema> OWNER TO <role_name>;
+-- 6) ===> Query to check the ownership of tables in a specific schema
+SELECT schemaname,
+       tablename,
+       tableowner
+FROM pg_tables
+WHERE schemaname NOT LIKE 'pg_%'
+AND schemaname <> 'information_schema'
+ORDER BY schemaname, tablename;
 
--- _______________________________________________________
--- 4 ===> Query to transfer ownwership of all schemas in the database to a new role;
-DO $$
+
+-- 7) ===> Query to transfer ownwership of all schemas in the database to a new role;
+DO $$ 
 DECLARE
     r RECORD;
 BEGIN
@@ -24,25 +51,43 @@ BEGIN
     LOOP
         EXECUTE 'ALTER SCHEMA ' 
         || quote_ident(r.nspname) 
-        || ' OWNER TO <role_name>';
+        || ' OWNER TO <role_name> ;';
     END LOOP; 
 END $$;
 
--- 5) ==> Transfer ownership of all tables in a schema
-DO $$
--- what's the function of two dollar signs here? The two dollar signs (`$$`) in PostgreSQL are used to define a string literal that can contain multiple lines and special characters without needing to escape them, eg this `DO` block. Without it each segment separated by (;) would be treated as a separate command, and the single quotes within the block would need to be escaped;
+
+-- 8) ===> Transfer ownership of all tables in a schema
+DO $$ -- The two dollar signs (`$$`) are used to define a string literal that can contain multiple lines and special characters without needing to escape them;
 DECLARE
     r RECORD;
 BEGIN
-    FOR r IN SELECT tablename FROM pg_tables WHERE schemaname = <schema> --schema name
+    FOR r IN SELECT tablename FROM pg_tables WHERE schemaname = <schema>
     LOOP -- LOOP over each r in tablename[];
-        EXECUTE 'ALTER TABLE <schema> '
-        || quote_ident(r.tablename)
-        || ' OWNER TO <role_name>'; --is EXECUTE necessary here? Yes, it initiates `EXECUTE` dynamic SQL query; The `quote_ident` function safely handles any special characters in the table names;
+        EXECUTE 'ALTER TABLE <schema_name>.' 
+        || quote_ident(r.tablename) 
+        || ' OWNER TO <role_name>;';
     END LOOP;
 END $$;
 
--- 4) ===> Grant READ/WRITE access on the schema and tables to the new role;
+
+-- 9) ===> Query to grant READ/WRITE access on all tables in all schemas to new role;
+DO $$ 
+DECLARE
+    r RECORD;
+BEGIN
+    FOR r IN SELECT nspname
+             FROM pg_namespace
+             WHERE nspname NOT LIKE 'pg_%' 
+               AND nspname <> 'information_schema' 
+    LOOP
+        EXECUTE 'GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA ' 
+        || quote_ident(r.nspname) 
+        || ' TO <role_name>;'; 
+    END LOOP; 
+END $$;
+
+
+-- 10) ===> Grant READ/WRITE access on the schema and tables to the new role;
 -- Grant permission to connect to the database
 GRANT CONNECT ON DATABASE <database> TO <role_name>;
 
@@ -51,10 +96,10 @@ GRANT USAGE, CREATE ON SCHEMA <schema> TO <role_name>;
 
 -- Grant SELECT, INSERT, UPDATE, DELETE on all existing tables in the schema
 -- Unlike ownership queries which require 'ALTER TABLE', You can 'GRANT' these permissions to all tables in a schema in one go;
-GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA <schema> TO <role_name>;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA <schema_name> TO <role_name>;
 
 -- Grant SELECT, INSERT, UPDATE, DELETE on all tables added in the future
-ALTER DEFAULT PRIVILEGES IN SCHEMA <schema> GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO <role_name>;
+ALTER DEFAULT PRIVILEGES IN SCHEMA <schema_name> GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO <role_name>;
 
 -- Grant USAGE on all sequences in the schema, to allow the role to use sequences for auto-incrementing columns;
 GRANT USAGE ON ALL SEQUENCES IN SCHEMA <schema> TO <role_name>;
